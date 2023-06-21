8dc1ce216f532658ce217908b7ff38a06fb0bdc5
javainfo-net <javainfopage@gmail.com>
2023-06-21 13:03:22 +0200

    add if statement and update others

---
+++ src/net/javainfo/basics/IfStatement.java
@@ -0,0 +1,27 @@
+package net.javainfo.basics;
+
+import java.awt.desktop.PrintFilesEvent;
+import java.util.Scanner;
+
+// Przykład użycia pętli warunkowych
+public class IfStatement {
+
+    public static void main(String[] args){
+
+        int age;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj swój wiek: ");
+        age = scanner.nextInt();
+
+        if(age < 18){
+            System.out.printf("Masz %d lat - %s",age,"nie jesteś pełnoletni/a.");
+        }
+        else if (age == 18){
+            System.out.println("Właśnie osiągnąłeś/aś pełnoletniość.");
+        }
+        else{
+            System.out.printf("Jesteś pełnoletni/a już od %d lat", age - 18);
+        }
+    }
+}
