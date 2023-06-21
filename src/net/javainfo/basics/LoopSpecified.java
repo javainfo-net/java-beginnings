7ddab4aa2198282d6569f5a5e1d9b2be2f8dfb96
javainfo-net <javainfopage@gmail.com>
2023-06-21 13:31:09 +0200

    add specified loop

---
+++ src/net/javainfo/basics/LoopSpecified.java
@@ -0,0 +1,12 @@
+package net.javainfo.basics;
+
+//Przykład użycia pętli określonej
+public class LoopSpecified {
+    public static void main(String[] args){
+
+        System.out.println("Zaczynamy odliczanie:");
+        for(int i = 10; i >= 0; --i){
+            System.out.printf("%d\n",i);
+        }
+    }
+}
